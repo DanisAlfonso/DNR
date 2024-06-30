941d332465b8758e432cbb89b4827c84f2af6d9a
Danis <danis.ramirez.hn@gmail.com>
2024-06-30 11:50:30 +0200

    first commit

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -1,39 +1,47 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <pybind11/numpy.h>
+#include "math_utilities.h"
 #include "lu_decomposition.h"
 
 namespace py = pybind11;
+
 using namespace DNR;
 
 PYBIND11_MODULE(DNR, m) {
-    m.doc() = "Python bindings for DNRR";
+    m.doc() = "Python bindings for MyProject";
 
-    py::class_<Matrix<double>>(m, "MatrixDouble")
+    m.def("test_function", []() {
+        return 42.0;
+    }, "A simple test function");
+
+    py::class_<MatrixDouble>(m, "MatrixDouble")
         .def(py::init<int, int>())
-        .def("nrows", &Matrix<double>::nrows)
-        .def("ncols", &Matrix<double>::ncols)
-        .def("__getitem__", [](const Matrix<double> &m, std::pair<size_t, size_t> i) {
+        .def("nrows", &MatrixDouble::nrows)
+        .def("ncols", &MatrixDouble::ncols)
+        .def("__getitem__", [](const MatrixDouble &m, std::pair<int, int> i) {
             return m[i.first][i.second];
         })
-        .def("__setitem__", [](Matrix<double> &m, std::pair<size_t, size_t> i, double v) {
+        .def("__setitem__", [](MatrixDouble &m, std::pair<int, int> i, double v) {
             m[i.first][i.second] = v;
         });
 
-    py::class_<Vector<double>>(m, "VectorDouble")
+    py::class_<VectorDouble>(m, "VectorDouble")
         .def(py::init<int>())
-        .def("size", &Vector<double>::size)
-        .def("__getitem__", [](const Vector<double> &v, size_t i) {
+        .def("size", &VectorDouble::size)
+        .def("__getitem__", [](const VectorDouble &v, int i) {
             return v[i];
         })
-        .def("__setitem__", [](Vector<double> &v, size_t i, double val) {
+        .def("__setitem__", [](VectorDouble &v, int i, double val) {
             v[i] = val;
         });
 
     py::class_<LU>(m, "LU")
-        .def(py::init<const Matrix<double>&>())
-        .def("solve", py::overload_cast<const Vector<double>&, Vector<double>&>(&LU::solve))
-        .def("solve_matrix", py::overload_cast<const Matrix<double>&, Matrix<double>&>(&LU::solve))
+        .def(py::init<const MatrixDouble&>())
+        .def("solve", (void (LU::*)(const VectorDouble&, VectorDouble&)) &LU::solve)
+        .def("solve_matrix", (void (LU::*)(const MatrixDouble&, MatrixDouble&)) &LU::solve)
+        .def("inverse", &LU::inverse)
         .def("det", &LU::det)
-        .def("inverse", &LU::inverse);
+        .def("luDecomposition", &LU::luDecomposition);
 }
 
